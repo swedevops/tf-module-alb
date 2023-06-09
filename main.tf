64365ee13646a7345a2e2c25dfb3728e3ee6d25b
swedev99 <swedevops99@gmail.com>
2023-06-09 16:54:26 +0530

    code dry

--- main.tf
+++ main.tf
@@ -4,23 +4,22 @@ resource "aws_security_group" "sg" {
   vpc_id      = var.vpc_id
 
   ingress {
-    description      = "HTTP"
-    from_port        = 80
-    to_port          = 80
-    protocol         = "tcp"
-    cidr_blocks      = var.allow_alb_cidr
-
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = var.allow_alb_cidr
   }
 
   egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = -1
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
 
-  tags = merge(var.tags, { name = "${var.name}-alb-${var.env}-sg" })
-
+  tags = merge(var.tags, { Name = "${var.name}-alb-${var.env}-sg" })
 }
 
 resource "aws_lb" "main" {
@@ -29,8 +28,9 @@ resource "aws_lb" "main" {
   load_balancer_type = "application"
   security_groups    = [aws_security_group.sg.id]
   subnets            = var.subnets
-  tags               = merge(var.tags, { name = "${var.name}-alb-${var.env}" })
+  tags               = merge(var.tags, { Name = "${var.name}-alb-${var.env}" })
 }
+
 resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_lb.main.arn
   port              = "80"
@@ -41,22 +41,9 @@ resource "aws_lb_listener" "main" {
 
     fixed_response {
       content_type = "text/plain"
-      message_body = "unauthorized"
+      message_body = "Unauthorized"
       status_code  = "403"
     }
   }
+
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
